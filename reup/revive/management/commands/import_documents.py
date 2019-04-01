ac6195aecab6171a85535a3b835252ffffe1f0e8
Alex Neamtu <alexneamtu@gmail.com>
2019-04-02 01:26:19 +0300

    19 - Recover FL1 Hoover document URLs - Manage.py csv import

--- reup/revive/management/commands/import_documents.py
+++ reup/revive/management/commands/import_documents.py
@@ -1,20 +1,36 @@
-from django.core.management.base import BaseCommand, CommandError
+import csv
+from pathlib import Path
+from django.core.management.base import BaseCommand
+from django.db import IntegrityError
+
+
+from ...models import Document
 
 
 class Command(BaseCommand):
-    help = 'Imports old documents to the database'
+    help = 'Imports old documents to the database from csv with the `id,md5,sha1` header.'
 
     def add_arguments(self, parser):
-        parser.add_argument('csv_path', nargs='+', type=int)
+        parser.add_argument('csv_path', type=Path, help='The csv to import.')
 
-    # def handle(self, *args, **options):
-        # for poll_id in options['poll_id']:
-        #     try:
-        #         poll = Document.objects.get(pk=poll_id)
-        #     except Document.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % poll_id)
-        #
-        #     poll.opened = False
-        #     poll.save()
-        #
-        #     self.stdout.write(self.style.SUCCESS('Successfully closed document "%s"' % poll_id))
+    def handle(self, *args, **options):
+        imported = 0
+        errors = 0
+        with open(options['csv_path']) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                try:
+                    _, created = Document.objects.get_or_create(
+                        old_id=row['id'],
+                        md5=row['md5'],
+                        sha1=row['sha1'],
+                    )
+                    imported += 1
+                except IntegrityError as e:
+                    errors += 1
+                    print(e)
+
+            if errors:
+                self.stdout.write(self.style.WARNING('Imported "%d" documents with "%d" errors' % (imported, errors)))
+            else:
+                self.stdout.write(self.style.SUCCESS('Successfully imported "%d" documents.' % imported))
--- reup/revive/models.py
+++ reup/revive/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Document(models.Model):
-    old_id = models.IntegerField()
+    old_id = models.IntegerField(unique=True)
     md5 = models.CharField(max_length=40, blank=True, db_index=True)
     sha1 = models.CharField(max_length=50, blank=True, db_index=True)
