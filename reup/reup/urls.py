0519f541136e579ba7858c4df4cc7119ac4bd843
Alex Neamtu <alexneamtu@gmail.com>
2019-04-04 02:19:07 +0300

    - Set `revive` as index

--- reup/reup/urls.py
+++ reup/reup/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('revive/', include('revive.urls')),
+    path('', include('revive.urls')),
     path('admin/', admin.site.urls),
 ]
