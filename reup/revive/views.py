827a1ddca60e66d9ef57f84d55d561ea7cc17971
Alex Neamtu <alexneamtu@gmail.com>
2019-06-23 23:32:47 +0300

    Parse full URLs - Updated captions to specify ID or url - Updated the controller to parse the first digits group in a string

--- reup/revive/views.py
+++ reup/revive/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render
 
 from .models import Document
@@ -7,11 +8,15 @@ def index(request):
     ctx = {}
 
     try:
-        if 'id' in request.POST:
-            ctx['document'] = Document.objects.get(old_id=request.POST['id'])
+        if 'input' in request.POST:
+            idMatch = re.search('\d+', request.POST['input'])
+            if idMatch is None:
+                raise ValueError('Could not find an ID in the input.')
+            print(idMatch[0])
+            ctx['document'] = Document.objects.get(old_id=idMatch[0])
     except Document.DoesNotExist:
         ctx['message'] = 'ID not found.'
-    except ValueError:
-        ctx['message'] = 'Invalid ID.'
+    except ValueError as e:
+        ctx['message'] = e
 
     return render(request, 'revive/index.html', ctx)
