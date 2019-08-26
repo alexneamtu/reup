4a88e20178b0bfa0dfd4b39d16c9bacc43b03452
Alex Morega <alex@grep.ro>
2019-08-26 17:18:45 +0300

    Reorganize the ifs

--- reup/revive/views.py
+++ reup/revive/views.py
@@ -5,34 +5,37 @@ from django.conf import settings
 
 from .models import Document
 
-def index(request):
-    ctx = {}
 
-    try:
-        if 'input' in request.POST:
-            document_search = urllib.parse.unquote(request.POST['input'])
+def match_url(document_search):
+        url_match = re.search(
+            r'https://.*/doc/(([A-Za-z0-9]+/[A-Fa-f0-9]{64})|(\?path=.*))',
+            document_search,
+        )
 
-            url_match = re.search(r'https://.*/doc/(([A-Za-z0-9]+/[A-Fa-f0-9]{64})|(\?path=.*))', document_search)
-            new_url = None
-            if url_match is not None:
-                new_url = urllib.parse.urljoin(settings.REUP_DOCUMENT_URL_PREFIX, url_match[1])
-
-            ctx['urls'] = {
-                'new_url': new_url
+        if url_match:
+            return {
+                'url': urllib.parse.urljoin(settings.REUP_DOCUMENT_URL_PREFIX,
+                                            url_match[1]),
             }
 
-            id_match = re.search(r'\d+', document_search)
-            if id_match is None:
-                raise ValueError('Could not find an ID in the input.')
+        id_match = re.search(r'\d+', document_search)
+        if id_match:
+            document = Document.objects.filter(old_id=id_match[0]).first()
 
-            ctx['document'] = Document.objects.get(old_id=id_match[0])
+            return {
+                'document': document,
+            }
 
-    except Document.DoesNotExist:
-        ctx['document'] = {
-            'md5': 'Not found',
-            'sha1': 'Not found',
+        return {
+            'error_message': 'Not Found',
         }
-    except ValueError as e:
-        ctx['message'] = e
+
+
+def index(request):
+    if request.POST:
+        ctx = match_url(request.POST['input'])
+
+    else:
+        ctx = {}
 
     return render(request, 'revive/index.html', ctx)
